629e26b00327104aa584fc6dd3d54acf506deaa4
张山 <zdk20001129@163.com>
2023-05-11 20:11:04 +0800

    feat: ✨ jvms+小乌龟

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -72,9 +72,9 @@ module.exports = {
                         items: [
                             { text: "IDEA", link: "/softwareInstallation/idea" },
                             { text: "GIt", link: "/softwareInstallation/git" },
-                            { text: "TortoiseGit小乌龟", link: "/softwareInstallation/git" },
-                            { text: "JVMS jdk管理", link: "/softwareInstallation/git" },
-                            { text: "NVM node管理", link: "/softwareInstallation/git" },
+                            { text: "TortoiseGit小乌龟", link: "/softwareInstallation/TortoiseGit" },
+                            { text: "JVMS jdk管理", link: "/softwareInstallation/jvms" },
+                            { text: "NVM node管理", link: "/softwareInstallation/nvm" },
                             { text: "JetBrains Toolbox", link: "/softwareInstallation/JetBrains-Toolbox" }
                         ]
                     },
